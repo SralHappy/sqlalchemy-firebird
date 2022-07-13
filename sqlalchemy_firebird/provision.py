d729cdd9ae587468782085b2577520982f8a18a5
SralHappy <92910239+SralHappy@users.noreply.github.com>
2022-07-13 14:36:55 +0200

    Update provision.py

--- sqlalchemy_firebird/provision.py
+++ sqlalchemy_firebird/provision.py
@@ -1,7 +1,7 @@
 from sqlalchemy.testing.provision import temp_table_keyword_args
 
 
-@temp_table_keyword_args.for_db("firebird")
+@temp_table_keyword_args.for_db("firebird-new")
 def _firebird_temp_table_keyword_args(cfg, eng):
     return {
         "prefixes": ["GLOBAL TEMPORARY"],
